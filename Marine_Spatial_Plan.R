3a06d389ddc696a93ef8c2918d4026e3b40f8783
Ben Wittenberg <benscottwittenberg@gmail.com>
2024-05-20 11:24:33 +0200

    Alpha diversity estimates

--- Marine_Spatial_Plan.R
+++ Marine_Spatial_Plan.R
@@ -5,6 +5,7 @@ FishSurvey <- read.csv("EstuaryFishSurveyData.csv")
 #Load packages
 library(vegan)
 library(tidyverse)
+library(ggplot2)
 
 #Now get alpha diversity for each estuary 
 FishSurvey[is.na(FishSurvey)] = 0
@@ -17,6 +18,43 @@ Alpha
 
 FishSurvey$Alpha <- Alpha
 
+#Let's order by the 20 most diverse estuaries: 
+FishSurvey <- FishSurvey[order(-FishSurvey$Alpha),]
+FishSurvey[1:20,1]
+#This gives us the estuaries with the highest species counts 
+#Mlalazi is the most diverse 
+
+Alphalist <- FishSurvey[1:20,1]
+
+#Let's plot this though 
+
+FishSurvey$kmEast <- (2947 - FishSurvey$kmWest)
+ggplot(FishSurvey, aes(kmEast,Alpha)) + 
+  geom_point()
+
+
+ggplot(FishSurvey, aes(kmEast,Alpha)) + 
+   geom_point(aes(colour = cut(Alpha, c(0,34,60))),
+              size = 2) +
+   scale_color_manual(name = "Alpha",
+                      values = c("(0,34]" = "blue",
+                                 "(34,60]" = "red"),
+                      labels = c("< 34 species","Top 20"))
+
+#Now let's try Colin's way: 
+FishSurvey$Alphalist <- 0
+FishSurvey$Alphalist[1:20] <- 1
+#I don't know man, I think cutting the data is easier and more flexible 
+
+#How many species occur in these top 20 estuaries? 
+sum(FishSurvey[1:20,149])
+#So the summed alpha is 760 for the 20 most diverse estuaries, but those will be many recounts of the same species 
+
+
+
+
+
+
 #Now we have alpha diversity for each estuary
 
 #Shall we try Shannon diversity? 
@@ -25,3 +63,13 @@ Shannon <- diversity(FishSurvey[,4:148])
 Shannon
 
 FishSurvey$Shannon <- Shannon
+
+#Now let's try for some Beta-diversity: just for the hell of it 
+
+Simpson <- diversity(FishSurvey[,4:148],"simpson")
+Simpson
+
+FishSurvey$Simpson <- Simpson
+
+#Now for Simpson's diversity, a low number = high diversity but a high number = low diversity, so should I invert it by subtracting it from 1? 
+
